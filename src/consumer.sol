513e3a01087ae51f6e1755956337454a4079e575
axatbhardwaj <axatbhardwaj@outlook.com>
2024-11-17 21:15:46 +0530

    consumer draft 1

--- src/consumer.sol
+++ src/consumer.sol
@@ -1,79 +1,134 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+pragma solidity ^0.8.0;
 
 import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
 import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 
-/// @title Anime Quote Fetcher
-/// @notice This contract fetches a random anime quote from the Animechan API
-/// @dev This contract uses Chainlink Functions to make HTTP GET requests
-contract AnimeQuoteFetcher is FunctionsClient, ConfirmedOwner {
+contract AnimeConsumer is FunctionsClient, ConfirmedOwner {
     using FunctionsRequest for FunctionsRequest.Request;
 
-    mapping(bytes32 => string) public requestIdToQuote;
-    mapping(bytes32 => string) public requestIdToAnimeName;
-    mapping(bytes32 => string) public requestIdToCharacterName;
-
-    // State variables to store the last request ID, response, and error
     bytes32 public s_lastRequestId;
     bytes public s_lastResponse;
     bytes public s_lastError;
-    uint64 public subscriptionId;
+    string public s_lastQuote;
+    string public s_lastCharacter;
+    string public s_lastAnime;
 
-    event QuoteFetched(bytes32 indexed requestId, string quote, string animeName, string characterName);
+    error UnexpectedRequestID(bytes32 requestId);
 
-    constructor(address oracle, uint64 _subscriptionId) FunctionsClient(oracle) ConfirmedOwner(msg.sender) {
-        subscriptionId = _subscriptionId;
-    }
+    event Response(bytes32 indexed requestId, bytes response, bytes err);
 
-    /// @notice Sends a request to fetch a random anime quote
-    function fetchQuote() public onlyOwner {
+    constructor(
+        address router
+    ) FunctionsClient(router) ConfirmedOwner(msg.sender) {}
+
+    string source =
+        "const apiResponse = await Functions.makeHttpRequest({"
+        "  url: `https://animechan.io/api/v1/quotes/random`"
+        "});"
+        "if (apiResponse.error) {"
+        "  console.error(apiResponse.error);"
+        "  throw Error('Request failed');"
+        "}"
+        "const { data } = apiResponse;"
+        "const quote = data.content;"
+        "const anime = data.anime.name;"
+        "const character = data.character.name;"
+        "return Functions.encodeString(quote + ' - ' + character + ' (' + anime + ')');";
+
+    /**
+     * @notice Send a simple request
+     * @param encryptedSecretsUrls Encrypted URLs where to fetch user secrets
+     * @param donHostedSecretsSlotID Don hosted secrets slotId
+     * @param donHostedSecretsVersion Don hosted secrets version
+     * @param args List of arguments accessible from within the source code
+     * @param bytesArgs Array of bytes arguments, represented as hex strings
+     * @param subscriptionId Billing ID
+     */
+    function sendRequest(
+        bytes memory encryptedSecretsUrls,
+        uint8 donHostedSecretsSlotID,
+        uint64 donHostedSecretsVersion,
+        string[] memory args,
+        bytes[] memory bytesArgs,
+        uint64 subscriptionId,
+        uint32 gasLimit,
+        bytes32 donID
+    ) external onlyOwner returns (bytes32 requestId) {
         FunctionsRequest.Request memory req;
-        req.initializeRequestForHttpGet("https://animechan.io/api/v1/quotes/random");
-        s_lastRequestId = sendRequest(req, subscriptionId);
-    }
-
-    /// @notice Callback function to handle the response from the Chainlink Functions
-    /// @param requestId The ID of the request
-    /// @param response The response from the API
-    /// @param error The error message, if any
-    function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory error) internal override {
-        s_lastRequestId = requestId;
-        s_lastResponse = response;
-        s_lastError = error;
-
-        if (error.length == 0) {
-            // Parse the response and extract the quote, anime name, and character name
-            (string memory quote, string memory animeName, string memory characterName) = parseQuote(response);
-            requestIdToQuote[requestId] = quote;
-            requestIdToAnimeName[requestId] = animeName;
-            requestIdToCharacterName[requestId] = characterName;
-            emit QuoteFetched(requestId, quote, animeName, characterName);
+        req.initializeRequestForInlineJavaScript(source);
+        if (encryptedSecretsUrls.length > 0)
+            req.addSecretsReference(encryptedSecretsUrls);
+        else if (donHostedSecretsVersion > 0) {
+            req.addDONHostedSecrets(
+                donHostedSecretsSlotID,
+                donHostedSecretsVersion
+            );
         }
+        if (args.length > 0) req.setArgs(args);
+        if (bytesArgs.length > 0) req.setBytesArgs(bytesArgs);
+        s_lastRequestId = _sendRequest(
+            req.encodeCBOR(),
+            subscriptionId,
+            gasLimit,
+            donID
+        );
+        return s_lastRequestId;
     }
 
-    /// @notice Parses the quote, anime name, and character name from the API response
-    /// @param response The response from the API
-    /// @return The extracted quote, anime name, and character name
-    function parseQuote(bytes memory response) internal pure returns (string memory, string memory, string memory) {
-        // Assuming the response is a JSON object with the quote in the "content" field,
-        // anime name in the "anime.name" field, and character name in the "character.name" field
-        // This is a simplified example, you may need to use a JSON parsing library
-        string memory quote = extractValue(response, "content");
-        string memory animeName = extractValue(response, "anime.name");
-        string memory characterName = extractValue(response, "character.name");
-        return (quote, animeName, characterName);
+    /**
+     * @notice Send a pre-encoded CBOR request
+     * @param request CBOR-encoded request data
+     * @param subscriptionId Billing ID
+     * @param gasLimit The maximum amount of gas the request can consume
+     * @param donID ID of the job to be invoked
+     * @return requestId The ID of the sent request
+     */
+    function sendRequestCBOR(
+        bytes memory request,
+        uint64 subscriptionId,
+        uint32 gasLimit,
+        bytes32 donID
+    ) external onlyOwner returns (bytes32 requestId) {
+        s_lastRequestId = _sendRequest(
+            request,
+            subscriptionId,
+            gasLimit,
+            donID
+        );
+        return s_lastRequestId;
     }
 
-    /// @notice Extracts a value from a JSON object
-    /// @param json The JSON object
-    /// @param key The key of the value to extract
-    /// @return The extracted value
-    function extractValue(bytes memory json, string memory key) internal pure returns (string memory) {
-        // This is a simplified example, you may need to use a JSON parsing library
-        // to properly extract values from the JSON object
-        return string(json); // Placeholder implementation
+    /**
+     * @notice Store latest result/error
+     * @param requestId The request ID, returned by sendRequest()
+     * @param response Aggregated response from the user code
+     * @param err Aggregated error from the user code or from the execution pipeline
+     * Either response or error parameter will be set, but never both
+     */
+    function fulfillRequest(
+        bytes32 requestId,
+        bytes memory response,
+        bytes memory err
+    ) internal override {
+        if (s_lastRequestId != requestId) {
+            revert UnexpectedRequestID(requestId);
+        }
+
+        // Assuming the response is encoded as (string, string, string)
+        (
+            string memory quote,
+            string memory character,
+            string memory anime
+        ) = abi.decode(response, (string, string, string));
+
+        // Save the destructured response to state variables
+        s_lastQuote = quote;
+        s_lastCharacter = character;
+        s_lastAnime = anime;
+
+        s_lastError = err;
+        emit Response(requestId, s_lastResponse, s_lastError);
     }
 }
